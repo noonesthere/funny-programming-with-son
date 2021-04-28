88f4b5b8df32dc3ef9686386f572c6216276f9e3
Dmitriy Zlobenets <dmitriy.zlobenets@intertechgroup.biz>
2021-04-28 14:36:20 +0300

    git test1

--- src/ua/com/dzlobenets/son/app/Main.java
+++ src/ua/com/dzlobenets/son/app/Main.java
@@ -5,7 +5,8 @@ import java.util.Map;
 import java.util.Objects;
 
 public class Main {
-
+    // line 1
+    // line 2
     private static final String[] MEMBERS = new String[]{
             "Mother", //0
             "Father",// 1
